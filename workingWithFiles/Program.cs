6c14b6afd0d2f6d8db06c0d50842c21ac7355dbd
Kurban <kurbanovkk96@mail.ru>
2023-10-05 19:51:20 +0300

    Добавьте файлы проекта.

---
+++ workingWithFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace workingWithFiles
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var wwf = new workWithFiles();
+            Directory.CreateDirectory("c:\\Otus\\TestDir1");
+            Directory.CreateDirectory("c:\\Otus\\TestDir2");
+            for (int i = 1; i <= 10; i++)
+            {
+                string fileName1 = $"c:\\Otus\\TestDir1\\File{i}.txt";
+                string fileName2 = $"c:\\Otus\\TestDir2\\File{i}.txt";
+                File.WriteAllText(fileName1, $"File {i} - Синхронная запись", Encoding.UTF8);
+                File.WriteAllText(fileName2, $"File {i} - Синхронная запись", Encoding.UTF8);
+                await wwf.AppendTextAsync(fileName1, DateTime.Now.ToString());
+                await wwf.AppendTextAsync(fileName2, DateTime.Now.ToString());
+            }
+            wwf.ReadAndPrintFiles("c:\\Otus\\TestDir1");
+            wwf.ReadAndPrintFiles("c:\\Otus\\TestDir2");
+        }
+    }
+}
---
+++ workingWithFiles/workWithFiles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingWithFiles
+{
+    internal class workWithFiles
+    {
+        public async Task AppendTextAsync(string fileName, string text)
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(fileName))
+                {
+                    await writer.WriteLineAsync(" - Асинхронное дополнение: " + text);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при асинхронной записи в файл {fileName}: {ex.Message}");
+            }
+        }
+
+        public void ReadAndPrintFiles(string directoryPath)
+        {
+            Console.WriteLine($"Содержимое директории: {directoryPath}");
+            Console.WriteLine();
+            string[] files = Directory.GetFiles(directoryPath);
+
+            foreach (string filePath in files)
+            {
+                try
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    string fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+                    Console.WriteLine($"{fileName}: {fileContent}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при чтении файла {filePath}: {ex.Message}");
+                }
+            }
+        }
+    }
+}
